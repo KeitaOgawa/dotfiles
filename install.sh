fc63920e5d22fd32176bb00b5efa86d6cf113928
KeitaOgawa <tsumi.toragi.kuzushi@gmail.com>
2019-05-01 13:55:49 +0900

    first commit

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DOT_DIRECTORY="${HOME}/dotfiles"
+REMOTE_URL="git@github.com:KeitaOgawa/dotfiles.git"
+DOT_FILES=(.bashrc .bash_profile .vimrc)
+
+for file in ${DOT_FILES[@]}
+do
+    ln -s ${DOT_DIRECTORY}/$file $HOME/$file
+done
