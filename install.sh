90bf857198868e5756443bcce355f5e7c514a917
Keita Ogawa <tsumi.toragi.kuzushi@gmail.com>
2019-05-01 14:50:56 +0900

    added .gitconfig

--- install.sh
+++ install.sh
@@ -2,7 +2,12 @@
 
 DOT_DIRECTORY="${HOME}/dotfiles"
 REMOTE_URL="git@github.com:KeitaOgawa/dotfiles.git"
-DOT_FILES=(.bashrc .bash_profile .vimrc)
+DOT_FILES=(
+    .bashrc
+    .bash_profile
+    .vimrc
+    .gitconfig
+    )
 
 for file in ${DOT_FILES[@]}
 do
